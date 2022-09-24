758755e2cb48e31c6438b4c64400ec2faee95b3f
Emil Koutanov <ekoutanov@gmail.com>
2022-09-25 08:43:55 +1000

    More docs

--- src/syllabus.rs
+++ src/syllabus.rs
@@ -1,12 +1,18 @@
+//! High-level organisation of learning material.
+
 use crate::topic::Topic;
 use itertools::Itertools;
 use std::collections::HashMap;
 
+/// The highest level of organisation. A syllabus specifies a set of named of courses that
+/// are available to students.
 pub struct Syllabus {
+    /// Mapping of course names to the courses.
     pub courses: HashMap<String, Course>,
 }
 
 impl Syllabus {
+    /// Obtains an ordered set of topic names that are taught across all courses in this syllabus.
     pub fn get_topic_names(&self) -> Vec<String> {
         self.courses
             .values()
@@ -18,11 +24,18 @@ impl Syllabus {
     }
 }
 
+/// A course specifies a set of named **modules**. A module is a [`Topic`] instance configured
+/// specifically for a course.
+///
+/// Note that a topic is a parametrised stream of questions. Topics such as addition may be
+/// taught in different grades, at different levels of difficulty.
 pub struct Course {
+    /// Mapping of module names to preconfigured topics.
     pub modules: HashMap<String, Box<dyn Topic>>,
 }
 
 impl Course {
+    /// Obtains an ordered set of topic names that are taught in this course.
     pub fn get_topic_names(&self) -> Vec<String> {
         self.modules
             .values()
@@ -82,3 +95,6 @@ pub mod presets {
         Box::new(t)
     }
 }
+
+#[cfg(test)]
+mod tests;
